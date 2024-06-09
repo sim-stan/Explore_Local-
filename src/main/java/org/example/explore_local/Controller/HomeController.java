64bea0d211c15f82e6898d119c5c5d33962fe376
sim-stan <simeon_stankov@hotmail.com>
2024-06-09 16:39:19 -0400

    Some Pages and Links Fixed

--- src/main/java/org/example/explore_local/Controller/HomeController.java
+++ src/main/java/org/example/explore_local/Controller/HomeController.java
@@ -2,26 +2,25 @@ package org.example.explore_local.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/users")
+
 public class HomeController {
 
 
     @GetMapping("/home")
-    public String home(){
+    public String home() {
         return "index";
     }
 
-    @GetMapping("/login")
-    public String login(){
-        return "login";
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
     }
 
-    @GetMapping("/register")
-    public String register(){
-        return "register";
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
     }
-
 }
