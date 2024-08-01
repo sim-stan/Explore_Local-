0724f9d1fc85a4576b2b3f71a1317b9896549024
sim-stan <simeon_stankov@hotmail.com>
2024-08-01 13:23:53 -0400

    -CitiesSeeder Class added -UserAndAdminSeeder Class added

---
+++ src/main/java/org/example/explore_local/initDB/CitiesSeeder.java
@@ -0,0 +1,22 @@
+package org.example.explore_local.initDB;
+
+import org.example.explore_local.service.CityService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CitiesSeeder implements CommandLineRunner {
+
+    private final CityService cityService;
+
+    public CitiesSeeder(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        cityService.seedCities();
+
+    }
+}
---
+++ src/main/java/org/example/explore_local/initDB/UserAndAdminSeeder.java
@@ -0,0 +1,21 @@
+package org.example.explore_local.initDB;
+
+import org.example.explore_local.service.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserAndAdminSeeder implements CommandLineRunner {
+
+    private final UserService userService;
+
+    public UserAndAdminSeeder(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        userService.seedUserAndAdmin();
+    }
+}
